1ade06ec8c3f248d66bb2c66f54ee8a7c5f8e659
Ai Search <admin@aisearchglobal.com>
2024-10-04 03:49:24 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,6 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 Study Assistant")
 
-
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.secrets["openai_api_key"]
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
